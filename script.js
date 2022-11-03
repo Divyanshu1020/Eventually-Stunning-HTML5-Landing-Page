9d4865fe36a66b2f984b1e57725db71b6200320b
Divyanshu Vishwakarman <97149795+Divyanshu1020@users.noreply.github.com>
2022-11-04 02:17:30 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById('form');
-const successElement = document.querySelector('.success');
-
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    successElement.innerHTML = `<i class="fa-solid fa-check"></i> Thank you!`;
-})
